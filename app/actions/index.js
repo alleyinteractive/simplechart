78526f0db74c49a2219e59203a22dd15acf18c4a
Josh Kadis <josh@alleyinteractive.com>
2016-07-14 15:22:41 -0400

    smarter data handling in API response

--- app/actions/index.js
+++ app/actions/index.js
@@ -45,13 +45,44 @@ export function bootstrapWidgetData(widgetId, fetchUrl) {
     function handleResponse(response) {
       return response.status === 200 ? response.json() : {};
     }
+
+    function handleJson(json) {
+      if (!json.success || !json.data) {
+        const errMsg = json.data && json.data.message ?
+          json.data.message : 'Unknown API error';
+        throw new Error(errMsg);
+      }
+
+      const apiData = {};
+      try {
+        apiData.data = JSON.parse(json.data.data);
+      } catch (err) {
+        console.log(err.message); // eslint-disable-line no-console
+        return;
+      }
+
+      try {
+        apiData.options = JSON.parse(json.data.options);
+      } catch (err) {
+        console.log(err.message); // eslint-disable-line no-console
+        return;
+      }
+
+      try {
+        apiData.metadata = JSON.parse(json.data.metadata);
+      } catch (err) {
+        console.log(err.message); // eslint-disable-line no-console
+        return;
+      }
+
+      dispatch(actionTrigger(
+        RECEIVE_API_DATA, { widget: widgetId, data: apiData }));
+    }
     /**
      * async data request
      */
     return fetch(fetchUrl)
       .then((response) => handleResponse(response))
-      .then((json) => dispatch(
-        actionTrigger(RECEIVE_API_DATA, { widget: widgetId, data: json }))
-      );
+      .then((json) => handleJson(json));
   };
 }
