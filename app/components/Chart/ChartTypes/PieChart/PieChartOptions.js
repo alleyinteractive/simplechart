b3892f90e5d5df9c79448b58fed272953a66b4d4
Josh Kadis <josh@alleyinteractive.com>
2016-07-23 15:06:08 -0400

    example of sending related prop

--- app/components/Chart/ChartTypes/PieChart/PieChartOptions.js
+++ app/components/Chart/ChartTypes/PieChart/PieChartOptions.js
@@ -2,6 +2,8 @@ import React from 'react';
 import GlobalOptions from '../../../ChartOptions/GlobalOptions';
 import { connect } from 'react-redux';
 import { Checkbox } from 'rebass';
+import actionTrigger from '../../../../actions';
+import { RECEIVE_CHART_OPTIONS } from '../../../../constants';
 
 class PieChartOptions extends GlobalOptions {
   constructor() {
@@ -9,14 +11,27 @@ class PieChartOptions extends GlobalOptions {
     this.changeHandlers = {
       donut: this._boolHandler,
     };
+    this._handleDonutChange = this._handleDonutChange.bind(this);
   }
+
+  _handleDonutChange(evt) {
+    this._handleChange(evt);
+    let donutRatio;
+    if (this._boolHandler(evt.target.value, 'donut')) {
+      donutRatio = 0.5;
+    } else {
+      donutRatio = 0;
+    }
+    this.props.dispatch(actionTrigger(RECEIVE_CHART_OPTIONS, { donutRatio }));
+  }
+
   childFields() {
     return (
         <Checkbox
           label="Donut chart"
           name="props-donut"
           checked={this.props.options.donut}
-          onChange={this._handleChange}
+          onChange={this._handleDonutChange}
         />
     );
   }
