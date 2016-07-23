b722ebe801d943d80cf34b9574a87f79300c1cd1
Josh Kadis <josh@alleyinteractive.com>
2016-07-23 14:58:16 -0400

    first child option

--- app/components/Chart/ChartTypes/PieChart/PieChartOptions.js
+++ app/components/Chart/ChartTypes/PieChart/PieChartOptions.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import GlobalOptions from '../../../ChartOptions/GlobalOptions';
 import { connect } from 'react-redux';
+import { Checkbox } from 'rebass';
 
 class PieChartOptions extends GlobalOptions {
-
+  constructor() {
+    super();
+    this.changeHandlers = {
+      donut: this._boolHandler,
+    };
+  }
+  childFields() {
+    return (
+        <Checkbox
+          label="Donut chart"
+          name="props-donut"
+          checked={this.props.options.donut}
+          onChange={this._handleChange}
+        />
+    );
+  }
 }
 
 PieChartOptions.propTypes = {
