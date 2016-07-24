34bac2571a9e170acadc11de956aeeaef55d4212
Josh Kadis <josh@alleyinteractive.com>
2016-07-24 11:17:33 -0400

    regular handler for donut changw

--- app/components/Chart/ChartTypes/PieChart/PieChartOptions.js
+++ app/components/Chart/ChartTypes/PieChart/PieChartOptions.js
@@ -2,11 +2,6 @@ import React from 'react';
 import GlobalOptions from '../../../ChartOptions/GlobalOptions';
 import { connect } from 'react-redux';
 import { Checkbox } from 'rebass';
-import actionTrigger from '../../../../actions';
-import {
-  RECEIVE_CHART_OPTIONS,
-  DELETE_CHART_OPTIONS,
-} from '../../../../constants';
 
 class PieChartOptions extends GlobalOptions {
   constructor() {
@@ -14,18 +9,6 @@ class PieChartOptions extends GlobalOptions {
     this.changeHandlers = {
       donut: this._boolHandler,
     };
-    this._handleDonutChange = this._handleDonutChange.bind(this);
-  }
-
-  _handleDonutChange(evt) {
-    this._handleChange(evt);
-    if (this._boolHandler(evt.target.value, 'donut')) {
-      this.props.dispatch(actionTrigger(
-        RECEIVE_CHART_OPTIONS, { donutRatio: 0.5 }));
-    } else {
-      this.props.dispatch(actionTrigger(
-        DELETE_CHART_OPTIONS, ['donutRatio', 'donut']));
-    }
   }
 
   childFields() {
@@ -34,7 +17,7 @@ class PieChartOptions extends GlobalOptions {
           label="Donut chart"
           name="props-donut"
           checked={this.props.options.donut}
-          onChange={this._handleDonutChange}
+          onChange={this._handleChange}
         />
     );
   }
