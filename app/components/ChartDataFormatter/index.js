2bf0c503eeadbce574cf58f3a109e689d414cc71
Josh Kadis <josh@alleyinteractive.com>
2016-07-28 00:01:17 -0400

    gonna start with assembling tickFormat from components

--- app/components/ChartDataFormatter/index.js
+++ app/components/ChartDataFormatter/index.js
@@ -4,16 +4,18 @@ import actionTrigger from '../../actions';
 import { dataIsMultiSeries } from '../../utils/misc';
 import { multiXY, singleXY } from '../../constants/chartXYFuncs';
 import { RECEIVE_CHART_OPTIONS } from '../../constants';
-import { Button, Select, Input } from 'rebass';
+import { Button, Input } from 'rebass';
 import update from 'react-addons-update';
 import NextPrevButton from '../Layout/RebassComponents/NextPrevButton';
 import createFormatter from '../../utils/createFormatter';
+import * as styles from './ChartDataFormatter.css';
 
 class ChartDataFormatter extends Component {
   constructor() {
     super();
     this._revert = this._revert.bind(this);
     this._update = this._update.bind(this);
+    this._handleChange = this._handleChange.bind(this);
     this.typeOptions = [
       {
         children: '',
@@ -55,7 +57,22 @@ class ChartDataFormatter extends Component {
       y: this.props.options.y || defaultInitial.y,
     };
 
-    this.setState({ multiSeries, initial, labels });
+    const formatterDefaults = {
+      prepend: '',
+      format: '',
+      multiplier: 1,
+      append: '',
+    };
+
+    this.setState({
+      multiSeries,
+      initial,
+      labels,
+      formatterSettings: {
+        x: formatterDefaults,
+        y: formatterDefaults,
+      },
+    });
   }
 
   /**
@@ -77,6 +94,18 @@ class ChartDataFormatter extends Component {
     this._update(axis, this.state.initial[axis]);
   }
 
+  _handleChange(evt) {
+    const field = evt.target.name.split('.');
+    const value = evt.target.type === 'number' ?
+      parseFloat(evt.target.value) : evt.target.value;
+    this.setState({ formatterSettings: update(this.state.formatterSettings,
+      { $apply: (settings) => {
+        settings[field[0]][field[1]] = value; // eslint-disable-line no-param-reassign
+        return settings;
+      } }
+    ) });
+  }
+
   render() {
     const revertX = () => this._revert('x');
     const revertY = () => this._revert('y');
@@ -92,20 +121,37 @@ class ChartDataFormatter extends Component {
             >Revert to default</Button>
           </div>
         ) : '' }
-        <div>
+        <div className={styles.fieldgroup}>
           <h4>{this.state.labels.y}</h4>
 
           <Input
             label="Prepend"
             name="y.prepend"
             rounded
+            value={this.state.formatterSettings.prepend}
+            onChange={this._handleChange}
           />
-
-          <Select
-            label="Type"
-            name="y.type"
+          <Input
+            label="D3 Format"
+            name="y.format"
             rounded
-            options={this.typeOptions}
+            value={this.state.formatterSettings.format}
+            onChange={this._handleChange}
+          />
+          <Input
+            label="Multiplier"
+            name="y.multiplier"
+            type="number"
+            value={this.state.formatterSettings.multiplier}
+            rounded
+            onChange={this._handleChange}
+          />
+          <Input
+            label="Append"
+            name="y.append"
+            value={this.state.formatterSettings.append}
+            rounded
+            onChange={this._handleChange}
           />
 
           <Button
