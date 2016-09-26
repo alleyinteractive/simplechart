07f0f24e2b1ef07f182d1575adc44ff30f35c581
Josh Kadis <josh@alleyinteractive.com>
2016-09-26 18:00:17 -0400

    check if custom palette has already been applied

--- app/middleware/chartOptionsInitMiddleware.js
+++ app/middleware/chartOptionsInitMiddleware.js
@@ -5,15 +5,15 @@ import {
 import { defaultPalette } from '../constants/defaultPalette';
 import actionTrigger from '../actions';
 
-export default function middleware() {
+export default function middleware(store) {
   return (next) => (action) => {
     const result = next(action);
     if (action.type !== RECEIVE_CHART_OPTIONS_INIT) {
       return result;
     }
 
-    // apply default D3 palette if none provided
-    if (!result.data.color) {
+    // apply default D3 palette if none provided or already set in store
+    if (!result.data.color && !store.getState().chartOptions.color) {
       result.data.color = defaultPalette;
       next(actionTrigger(
         RECEIVE_CHART_OPTIONS,
