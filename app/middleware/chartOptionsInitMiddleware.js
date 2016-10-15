b246302f2b65e2a5cbd702925409cfa7855df643
Josh Kadis <josh@alleyinteractive.com>
2016-10-14 21:21:26 -0400

    refactor chartOptionsInitMiddleware

--- app/middleware/chartOptionsInitMiddleware.js
+++ app/middleware/chartOptionsInitMiddleware.js
@@ -1,26 +1,21 @@
 import {
-  RECEIVE_CHART_OPTIONS,
   RECEIVE_CHART_OPTIONS_INIT,
 } from '../constants';
 import { defaultPalette } from '../constants/defaultPalette';
 import actionTrigger from '../actions';
 
-export default function middleware(store) {
-  return (next) => (action) => {
-    const result = next(action);
+export default function middleware({ getState }) {
+  return (dispatch) => (action) => {
     if (action.type !== RECEIVE_CHART_OPTIONS_INIT) {
-      return result;
+      return dispatch(action);
     }
 
     // apply default D3 palette if none provided or already set in store
-    if (!result.data.color && !store.getState().chartOptions.color) {
-      result.data.color = defaultPalette;
-      next(actionTrigger(
-        RECEIVE_CHART_OPTIONS,
-        result.data
-      ));
+    const nextActionData = action.data;
+    if (!action.data.color && !getState().chartOptions.color) {
+      nextActionData.color = defaultPalette;
     }
 
-    return result;
+    return dispatch(actionTrigger(action.type, nextActionData));
   };
 }
