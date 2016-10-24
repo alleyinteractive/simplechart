10d817f1e8c0962b62cd83784992277861e1c117
Josh Kadis <josh@alleyinteractive.com>
2016-10-24 10:57:30 -0400

    force chart type re-rendering

--- app/components/Chart/ChartTypes/NVD3Single/index.js
+++ app/components/Chart/ChartTypes/NVD3Single/index.js
@@ -1,48 +1,39 @@
 import React, { Component } from 'react';
 import NVD3Chart from 'react-nvd3';
 import update from 'react-addons-update';
-import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import actionTrigger from '../../../../actions';
 import { RECEIVE_ERROR } from '../../../../constants';
 
 class NVD3Single extends Component {
 
-  componentWillReceiveProps(nextProps) {
-    // If chart type changes, remove obsolete charts after render
+  componentWillMount() {
     this.setState({
-      cleanupNVD3Render: false,
-      nextType: nextProps.options.type,
+      datum: this._dataTransform(this.props.options.type, this.props.data),
     });
   }
 
-  componentDidUpdate() {
-    if (this.state.cleanupNVD3Render) {
-      this._cleanupNVD3Render(this.state.nextType, this.refs.chartNode);
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      datum: this._dataTransform(nextProps.options.type, nextProps.data),
+    });
+
+    try {
+      // Force the react-nvd3 to re-render the chart, when new props are received
+      this.refs.chartNode.rendering = true;
+      const wrapEl = this.refs.chartNode.refs.root.querySelector('g.nv-wrap');
+      if (wrapEl) {
+        wrapEl.parentNode.removeChild(wrapEl);
+      }
+    } catch (err) {
+      this.props.dispatch(actionTrigger(RECEIVE_ERROR, 'e004'));
+      console.log(err.message); // eslint-disable-line no-console
     }
   }
 
   /**
-   * We may need to manually remove old rendered charts when chart type changes
-   * from div.nv-chart > svg > g.nv-{prevChartType}
-   *
-   * @param string nextType Newly rendered chart type
-   * @param node refNode DOM node where chart are rendered
+   * Apply any special formatting specfic to a chart type
    */
-  _cleanupNVD3Render(nextType, refNode) {
-    try {
-      const chartNode = ReactDOM.findDOMNode(refNode);
-      chartNode.querySelectorAll('svg > g').forEach((typeEl) => {
-        // className is SVGAnimatedString so we check its baseVal
-        if (-1 === typeEl.className.baseVal.indexOf(`nv-${nextType}`)) {
-          chartNode.firstElementChild.removeChild(typeEl);
-        }
-      });
-    } catch (e) {
-      this.props.dispatch(actionTrigger(RECEIVE_ERROR, 'e004'));
-    }
-  }
-
   _dataTransform(chartType, data) {
     switch (chartType) {
       case 'discreteBarChart':
@@ -59,7 +50,7 @@ class NVD3Single extends Component {
   render() {
     return React.createElement(NVD3Chart, update(
       this.props.options, { $merge: {
-        datum: this._dataTransform(this.props.options.type, this.props.data),
+        datum: this.state.datum,
         ref: 'chartNode',
       } }
     ));
