5ba541cd341abcbf59e871023afd3a54d8ad0254
Josh Kadis <josh@alleyinteractive.com>
2016-11-11 08:36:23 -0500

    hrm

--- app/components/ChartDataFormatter/index.js
+++ app/components/ChartDataFormatter/index.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import NextPrevButton from '../Layout/RebassComponents/NextPrevButton';
 import { locales, defaultLocaleIndex } from '../../constants/d3Locales';
-import { RECEIVE_CHART_OPTIONS } from '../../constants';
+import { RECEIVE_CHART_OPTIONS_EXTEND } from '../../constants';
 import DispatchField from '../lib/DispatchField';
 
 class ChartDataFormatter extends Component {
   componentWillMount() {
     this.setState({
       revertTo: this.props.tickFormatSettings,
-      settings: this.props.tickFormatSettings,
-      currentLocale:
-        this.props.tickFormatSettings.locale || defaultLocaleIndex,
-    });
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      settings: nextProps.tickFormatSettings,
-      currentLocale:
-        nextProps.tickFormatSettings.locale || defaultLocaleIndex,
     });
   }
 
@@ -35,13 +24,24 @@ class ChartDataFormatter extends Component {
       <div>
         <div>
           <DispatchField
-            action={ RECEIVE_CHART_OPTIONS }
+            action={ RECEIVE_CHART_OPTIONS_EXTEND }
             fieldType="Select"
             fieldProps={{
               label: 'Locale',
               name: 'tickFormatSettings.locale',
               options: this._localeOptions(),
-              value: parseInt(this.state.currentLocale),
+              value: parseInt(
+                this.props.tickFormatSettings.locale || defaultLocaleIndex, 10),
+            }}
+          />
+
+          <DispatchField
+            action={ RECEIVE_CHART_OPTIONS_EXTEND }
+            fieldType="Checkbox"
+            fieldProps={{
+              label: 'Show currency symbol?',
+              name: 'tickFormatSettings.showCurrencySymbol',
+              checked: this.props.tickFormatSettings.showCurrencySymbol,
             }}
           />
         </div>
