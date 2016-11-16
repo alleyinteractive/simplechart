1b365b4b10ada0dc0775ca4526bfa473f89abc90
Josh Kadis <josh@alleyinteractive.com>
2016-11-16 15:26:36 -0500

    chart renders from active breakpoint!

--- app/components/Chart/index.js
+++ app/components/Chart/index.js
@@ -35,19 +35,45 @@ class Chart extends Component {
       });
   }
 
+  _getActiveDimensions(breakpoints) {
+    if (undefined === breakpoints ||
+      !breakpoints.values ||
+      !breakpoints.values.length
+    ) {
+      /**
+       * @todo Real default value here!
+       */
+      return { width: 395, height: 415 };
+    }
+
+    const idx = breakpoints.hasOwnProperty('active') ? breakpoints.active : 0;
+    return {
+      width: breakpoints.values[idx].maxWidth,
+      height: breakpoints.values[idx].height,
+    };
+  }
+
   render() {
     if (!this.state.chartTypeComponent) {
       return null;
     }
 
+    const activeDimensions = this._getActiveDimensions(
+      this.props.options.breakpoints);
+
     const chartTypeComponent = React.createElement(
       this.state.chartTypeComponent,
-      update(this.props, { $merge: { ref: 'chartTypeComponent' } })
+      update(this.props, {
+        ref: { $set: 'chartTypeComponent' },
+        options: {
+          height: { $set: activeDimensions.height },
+        },
+      })
     );
 
     return React.createElement(
       this.props.rulers ? RuledBox : 'div', // element type
-      this.props.rulers ? { width: 395, height: 415 } : {}, // props
+      this.props.rulers ? activeDimensions : {}, // props
       chartTypeComponent // children
     );
   }
