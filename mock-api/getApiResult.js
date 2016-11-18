eac195b618b05cd399f500a5e945fd1007e9d462
Josh Kadis <josh@alleyinteractive.com>
2016-11-18 16:53:33 -0500

    use baby parse on server side and mock meta data

--- mock-api/getApiResult.js
+++ mock-api/getApiResult.js
@@ -1,9 +1,12 @@
+import fs from 'fs';
+import path from 'path';
 import {
   getChartTypeDefaultOpts,
   getChartTypeObject,
 } from '../app/utils/chartTypeUtils';
-import { sampleData } from '../app/constants/sampleData';
 import { parseRawData, transformParsedData } from '../app/utils/rawDataHelpers';
+import Baby from 'babyparse';
+import mockMetadata from './mockMetadata';
 
 /**
  * Create mock API response
@@ -12,7 +15,7 @@ export default function getApiResult(chartType) {
   return {
     data: _getData(chartType),
     options: _getOptions(chartType),
-    // metadata: _getMockMetaData(chartType),
+    metadata: _getMockMetadata(chartType),
   };
 }
 
@@ -23,9 +26,25 @@ function _getOptions(chartType) {
   return getChartTypeDefaultOpts(chartType);
 }
 
+/**
+ * Get transformed chart data object
+ */
 function _getData(chartType) {
-  const dataFormat = getChartTypeObject(chartType).dataFormat;
-  const rawData = 'nvd3SingleSeries' === dataFormat ? sampleData[0].data : sampleData[1].data;
-  const parserResult = parseRawData(rawData);
+  const dataFormat = getChartTypeObject(chartType).config.dataFormat;
+  const rawData = _getRawData(
+    'nvd3SingleSeries' === dataFormat ? 'singleSeries.csv' : 'countriesByYear.csv');
+  const parserResult = parseRawData(Baby, rawData);
   return transformParsedData(parserResult[0], parserResult[1])[dataFormat];
+}
+
+/**
+ * Get contents of sample CSV data
+ */
+function _getRawData(samplename) {
+  const dataPath = path.join(__dirname, '../app/constants/sampleData', samplename);
+  return fs.readFileSync(dataPath, 'utf8');
+}
+
+function _getMockMetadata(chartType) {
+  return mockMetadata[chartType];
 }
