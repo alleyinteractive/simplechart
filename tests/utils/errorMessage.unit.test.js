1d7f9d2f51bdacf61dbb74df8f54cf62eaac513e
Davis Shaver <davis@alleyinteractive.com>
2016-11-23 20:07:52 -0500

    Updates to react packages

--- tests/utils/errorMessage.unit.test.js
+++ tests/utils/errorMessage.unit.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ErrorMessage from '../../app/utils/errorMessage.js';
-// import renderer from 'react-test-renderer';
-// import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 test('placeholder test', () => {
   expect('TK').toBe('TK');
