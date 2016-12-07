79d08c71014a040f6cc2049e173d447eba2fb437
Davis Shaver <davis@alleyinteractive.com>
2016-12-07 10:27:52 -0500

    Slight refactoring of nightwatch setup

---
+++ nightwatch.conf.js
@@ -0,0 +1,53 @@
+const SCREENSHOT_PATH = "./tests/artifacts/screenshots/";
+const BINPATH = './node_modules/nightwatch/bin/';
+
+module.exports = {
+  "src_folders" : ["./tests/integration"],
+  "output_folder" : "./tests/artifacts/reports",
+  "selenium" : {
+    "start_process" : true,
+    "server_path" : "./node_modules/nightwatch/bin/selenium.jar",
+    "host": "127.0.0.1",
+    "port" : 4444,
+    "cli_args" : {
+      "webdriver.chrome.driver" : "./node_modules/nightwatch/bin/chromedriver"
+    }
+  },
+  "test_settings" : {
+    "default" : {
+      "globals": {
+        "waitForConditionTimeout": 5000
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      }
+    }
+  }
+}
+
+require('fs').stat(BINPATH + 'selenium.jar', function (err, stat) { // got it?
+  if (err || !stat || stat.size < 1) {
+    require('selenium-download').ensure(BINPATH, function(error) {
+      if (error) throw new Error(error); // no point continuing so exit!
+      console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
+    });
+  }
+});
+
+
+function padLeft (count) {
+  return count < 10 ? '0' + count : count.toString();
+}
+
+var FILECOUNT = 0;
+function imgpath (browser) {
+  var a = browser.options.desiredCapabilities;
+  var meta = [];
+  meta.push(a.browserName ? a.browserName : 'any');
+  meta.push(a.name); // this is the test filename so always exists.
+  var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
+  return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
+}
+
+module.exports.imgpath = imgpath;
+module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
--- tests/integration/simple.js
+++ tests/integration/simple.js
@@ -1,10 +1,12 @@
+var conf = require('../../nightwatch.conf.js');
+
 module.exports = {
 	'Simplechart Assert Title': function(browser) {
 		browser
 			.url('http://localhost:8080')
 			.waitForElementVisible('body')
 			.assert.title('Simplechart')
-			.saveScreenshot('tests/artifacts/screenshots/simplechart.png')
+			.saveScreenshot(conf.imgpath(browser) + 'simplechart.png')
 			.end();
 	}
 };
