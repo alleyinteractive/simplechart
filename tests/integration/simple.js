e8277113b79b3900c5e60915963b539a78faa1fb
Davis Shaver <davis@alleyinteractive.com>
2016-12-11 14:37:38 -0500

    Add test for loading sample data

--- tests/integration/simple.js
+++ tests/integration/simple.js
@@ -1,12 +1,27 @@
 var conf = require('../../nightwatch.conf.js');
 
 module.exports = {
-	'Simplechart Assert Title': function(browser) {
+	'Simplechart assert title': function(browser) {
 		browser
 			.url('http://localhost:8080')
 			.waitForElementVisible('body')
 			.assert.title('Simplechart')
 			.saveScreenshot(conf.imgpath(browser) + 'simplechart.png')
+			.end();
+	},
+	'Simplechart assert sample data': function(browser) {
+		browser
+			.useXpath()
+			.url('http://localhost:8080')
+			.waitForElementVisible('//body')
+			.waitForElementVisible('//select[@name="sample-data-select"]')
+			.click('//select[@name="sample-data-select"] //option[@value="1"]')
+			.waitForElementVisible('(//button[@class="Button"])[3]')
+			.click('(//button[@class="Button"])[3]')
+			.assert.containsText(
+				'//p[@class="Text"]',
+				'Data input successful'
+			)
 			.end();
 	}
 };
