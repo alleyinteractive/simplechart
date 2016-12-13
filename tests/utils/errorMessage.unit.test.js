e6a4a0affdb9873215186c1201ada6a92460a304
Davis Shaver <davis@alleyinteractive.com>
2016-12-13 10:46:55 -0500

    Show expected result

--- tests/utils/errorMessage.unit.test.js
+++ tests/utils/errorMessage.unit.test.js
@@ -6,7 +6,7 @@ import ReactTestUtils from 'react-addons-test-utils';
 import { shallow } from 'enzyme';
 
 // Enzyme shallow
-describe('Basic <errorMessage />', () => {
+describe('Basic <ErrorMessage />', () => {
   const wrapper = shallow(<ErrorMessage />);
   const style = { marginTop: '65px' };
   it('It should contain a message element', function () {
@@ -17,11 +17,13 @@ describe('Basic <errorMessage />', () => {
     expect(wrapper.prop('rounded')).toBe(true);
     expect(wrapper.prop('theme')).toBe('error');
     expect(wrapper.prop('style')).toEqual(style);
-    // @TODO Diagnose why default message doesn't return
+    expect(wrapper.props().children).toEqual(
+        { children: 'An error occurred.' }
+    );
   });
 });
 
-describe('Custom <errorMessage />', () => {
+describe('Custom <ErorMessage />', () => {
   const wrapper = shallow(<ErrorMessage>Custom error.</ErrorMessage>);
   const style = { marginTop: '65px' };
   it('It should contain a message element', function () {
@@ -35,12 +37,11 @@ describe('Custom <errorMessage />', () => {
     expect(wrapper.props().children).toEqual(
         { children: 'Custom error.' }
     );
-    // @TODO Diagnose why default message doesn't return
   });
 });
 
 // React test utils
-describe('<errorMessage />', () => {
+describe('<ErrorMessage />', () => {
   const style = { marginTop: '65px' };
   const renderer = ReactTestUtils.createRenderer();
   renderer.render(<ErrorMessage />);
@@ -51,21 +52,4 @@ describe('<errorMessage />', () => {
     expect(obj.props['theme']).toBe('error');
     expect(obj.props['style']).toEqual(style);
   })
-});
-
-describe('Custom <errorMessage />', () => {
-  const wrapper = shallow(<ErrorMessage>Custom error.</ErrorMessage>);
-  const style = { marginTop: '65px' };
-  it('It should contain a message element', function () {
-    expect(wrapper.text()).toBe('<Message />');
-  });
-  it('It should have the default props & a custom message', function () {
-    expect(wrapper.prop('inverted')).toBe(true);
-    expect(wrapper.prop('rounded')).toBe(true);
-    expect(wrapper.prop('theme')).toBe('error');
-    expect(wrapper.prop('style')).toEqual(style);
-    expect(wrapper.props().children).toEqual(
-        { children: 'Custom error.' }
-    );
-  });
 });
