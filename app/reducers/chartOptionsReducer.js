eb5708d6226c3363184d07b0e939b39478fa8331
Max Eddy <max@alleyinteractive.com>
2017-05-25 16:40:52 -0400

    Some chart option reducer tests.

--- app/reducers/chartOptionsReducer.js
+++ app/reducers/chartOptionsReducer.js
@@ -3,24 +3,21 @@ import merge from 'lodash/fp/merge';
 import { RECEIVE_CHART_OPTIONS, RECEIVE_CHART_TYPE } from '../constants';
 
 export default function chartOptionsReducer(state = {}, action) {
-  let newState = Object.assign({ chartOptions: {} }, state);
+  const newState = Object.assign({ chartOptions: {} }, state);
 
   switch (action.type) {
     case RECEIVE_CHART_OPTIONS: {
-      newState = merge(newState, { chartOptions: action.data });
-      break;
+      return merge(newState, { chartOptions: action.data });
     }
 
     case RECEIVE_CHART_TYPE: {
-      const [xLabel, yLabel] = state.dataFields;
+      const [, xLabel, yLabel] = state.dataFields;
       const chartTypeConfig = action.data.config;
+      const hasChanged = state.chartType.type !== chartTypeConfig.type;
+      const isScatter = 'nvd3ScatterMultiSeries' === chartTypeConfig.dataFormat;
 
-      const shouldSetDefaultAxisLabels =
-        state.chartType.type !== chartTypeConfig.type &&
-        'nvd3ScatterMultiSeries' === chartTypeConfig.dataFormat;
-
-      if (shouldSetDefaultAxisLabels) {
-        newState = merge(state, {
+      if (hasChanged && isScatter) {
+        return merge(newState, {
           chartOptions: {
             xAxis: {
               axisLabel: xLabel,
--- tests/reducers/chartOptions.unit.test.js
+++ tests/reducers/chartOptions.unit.test.js
@@ -1,3 +1,65 @@
-test('', () => {
+import { RECEIVE_CHART_OPTIONS, RECEIVE_CHART_TYPE } from '../../app/constants';
+import reduce from '../../app/reducers/chartOptionsReducer';
 
+test('merges chart options ', () => {
+  const state = {
+    chartOptions: {
+      type: 'scatterChart',
+      showLegend: true,
+      color: [],
+    },
+  };
+
+  const action = {
+    type: RECEIVE_CHART_OPTIONS,
+    data: {
+      type: 'bubbleChart',
+      showLegend: true,
+      showControls: false,
+      color: ['#1f77b4'],
+    },
+  };
+
+  const expected = {
+    chartOptions: {
+      type: 'bubbleChart',
+      showLegend: true,
+      showControls: false,
+      color: ['#1f77b4'],
+    },
+  };
+
+  expect(reduce(state, action)).toEqual(expected);
+});
+
+test('merges axis labels if selecting scatter', () => {
+  const state = {
+    chartOptions: {},
+    chartType: {
+      type: 'bubbleChart',
+    },
+    dataFields: ['Foo', 'Bar', 'Baz'],
+  };
+
+  const action = {
+    type: RECEIVE_CHART_TYPE,
+    data: {
+      config: {
+        type: 'scatterChart',
+        dataFormat: 'nvd3ScatterMultiSeries',
+      },
+    },
+  };
+  const expected = {
+    chartOptions: {
+      xAxis: {
+        axisLabel: 'Bar',
+      },
+      yAxis: {
+        axisLabel: 'Baz',
+      },
+    },
+  };
+
+  expect(reduce(state, action)).toMatchObject(expected);
 });
