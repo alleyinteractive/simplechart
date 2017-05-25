dba6f3aae99cc3d8c23a171ac78856f089db88af
Max Eddy <max@alleyinteractive.com>
2017-05-25 10:42:39 -0400

    Create data transformer for scatter/bubble charts.

---
+++ app/utils/dataFormats/nvd3ScatterMultiSeries.js
@@ -0,0 +1,47 @@
+import compose from 'lodash/fp/flow';
+import groupBy from 'lodash/fp/groupBy';
+import map from 'lodash/fp/map';
+import { parse as parseDate } from '../parseDate';
+
+export default function transform(data, fields, dateFormat) {
+  // groups,x,y or groups,x,y,z
+  const isInvalidNumFields = -1 === [3, 4].indexOf(fields.length);
+  if (isInvalidNumFields) {
+    return false;
+  }
+
+  const [groupingLabel, xAxisLabel, yAxisLabel, sizeLabel] = fields;
+
+  const createPoint = (row) => {
+    const getSize = (size) => (size ? Math.round(size) / 100 : 1);
+    const isDateAxis = dateFormat.enabled && dateFormat.validated;
+
+    const xValue = isDateAxis ?
+      parseDate(row[xAxisLabel]) :
+      parseFloat(row[xAxisLabel]);
+
+    return {
+      x: xValue,
+      y: parseFloat(row[yAxisLabel]),
+      size: getSize(row[sizeLabel]) || 1,
+    };
+  };
+
+  const createSeries = (dataGroup) => ({
+    key: dataGroup[0][groupingLabel],
+    values: dataGroup.map(createPoint),
+  });
+
+  const validateSeries = (series) => {
+    const validate = ({ x, y }) => !x || isNaN(x) || !y || isNaN(y);
+    return series.find(validate) || false;
+  };
+
+  const getSeries = compose(
+    groupBy(groupingLabel),
+    map(createSeries),
+    validateSeries
+  );
+
+  return getSeries(data);
+}
--- app/utils/rawDataHelpers.js
+++ app/utils/rawDataHelpers.js
@@ -32,6 +32,7 @@ export function parseRawData(parser, rawData) {
  * setup transformed for dataFormats
  */
 export function transformParsedData(parsedData, parsedFields, dateFormat) {
+  console.log(parsedData, parsedFields, dateFormat);
   const transformed = {};
   Object.keys(dataTransformers).forEach((type) => {
     transformed[type] = dataTransformers[type](
@@ -50,7 +51,7 @@ export function validateTransformedData(transformedDataMap) {
     .every(isBadTransform);
 
   if (failedToTransform) {
-    return ['Field 1 must contain valid dates or numbers for multiple series data.'];
+    return ['Data is not valid for any chart types.'];
   }
 
   return [];
