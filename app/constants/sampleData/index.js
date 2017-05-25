ebaad138239553ae95cdc4c46e38fa63a0b180d1
Max Eddy <max@alleyinteractive.com>
2017-05-25 14:56:38 -0400

    Add some test data for scatter/bubble.

--- app/constants/sampleData/index.js
+++ app/constants/sampleData/index.js
@@ -2,6 +2,8 @@ import singleSeries from 'raw!./singleSeries.csv';
 import countriesByYear from 'raw!./countriesByYear.csv';
 import emissionsPerCountry from 'raw!./emissionsPerCountry.csv';
 import stockMarkets from 'raw!./stockMarkets.csv';
+import engineMpg from 'raw!./engineMpg.csv';
+import products from 'raw!./productsRevenueRatingProductionCost.csv';
 
 export const sampleData = [
   {
@@ -19,5 +21,13 @@ export const sampleData = [
   {
     label: 'Stock market indices, November 2016',
     data: stockMarkets,
+  },
+  {
+    label: 'Engine City and Highway MPG Comparison (scatter)',
+    data: engineMpg,
+  },
+  {
+    label: 'Product Revenue, Rating, and Product Cost Comparison (bubble)',
+    data: products,
   },
 ];
