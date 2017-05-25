358f72a4e737fced902b3b414fb9c8720d003d8b
Max Eddy <max@alleyinteractive.com>
2017-05-25 11:49:18 -0400

    Delete DELETE_CHART_OPTIONS action, because it's not being used anywhere.

--- app/reducers/chartOptionsReducer.js
+++ app/reducers/chartOptionsReducer.js
@@ -1,43 +1,19 @@
+import merge from 'lodash/fp/merge';
+
 import {
   RECEIVE_CHART_OPTIONS,
   RECEIVE_CHART_OPTIONS_EXTEND,
-  DELETE_CHART_OPTIONS,
 } from '../constants';
 import update from 'immutability-helper';
-import deepExtend from 'deep-extend';
 
 export default function chartOptionsReducer(state = {}, action) {
-  // Loop through array of keys passed via DELETE_CHART_OPTIONS
-  // and delete from state if defined
-  function _deleteKeys(applyState) {
-    if (!action.data || !action.data.length) {
-      return applyState;
-    }
-    action.data.forEach((key) => {
-      if ('undefined' !== typeof applyState[key]) {
-        delete applyState[key]; // eslint-disable-line no-param-reassign
-      }
-    });
-    return applyState;
-  }
-
-  function _extendState(applyState) {
-    const newState = update({}, { $set: applyState });
-    deepExtend(newState, action.data);
-    return newState;
-  }
-
   switch (action.type) {
     case RECEIVE_CHART_OPTIONS: {
       return update(state, { $merge: action.data });
     }
 
     case RECEIVE_CHART_OPTIONS_EXTEND:
-      return update(state, { $apply: _extendState });
-
-    case DELETE_CHART_OPTIONS: {
-      return update(state, { $apply: _deleteKeys });
-    }
+      return merge(state, action.data);
 
     default:
       return state;
