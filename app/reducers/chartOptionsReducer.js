716166133bc7d1ddd2caff74deb005f113090158
Max Eddy <max@alleyinteractive.com>
2017-05-25 14:47:27 -0400

    Setup reducer to set default axis labels for scatter/bubble.

--- app/reducers/chartOptionsReducer.js
+++ app/reducers/chartOptionsReducer.js
@@ -1,16 +1,41 @@
 import merge from 'lodash/fp/merge';
 
-import { RECEIVE_CHART_OPTIONS } from '../constants';
+import { RECEIVE_CHART_OPTIONS, RECEIVE_CHART_TYPE } from '../constants';
 
 export default function chartOptionsReducer(state = {}, action) {
-  const newState = Object.assign({ chartOptions: {} }, state);
+  let newState = Object.assign({ chartOptions: {} }, state);
 
   switch (action.type) {
     case RECEIVE_CHART_OPTIONS: {
-      return merge(newState, { chartOptions: action.data });
+      newState = merge(newState, { chartOptions: action.data });
+      break;
+    }
+
+    case RECEIVE_CHART_TYPE: {
+      const [xLabel, yLabel] = state.dataFields;
+      const chartTypeConfig = action.data.config;
+
+      const shouldSetDefaultAxisLabels =
+        state.chartType.type !== chartTypeConfig.type &&
+        'nvd3ScatterMultiSeries' === chartTypeConfig.dataFormat;
+
+      if (shouldSetDefaultAxisLabels) {
+        newState = merge(state, {
+          chartOptions: {
+            xAxis: {
+              axisLabel: xLabel,
+            },
+            yAxis: {
+              axisLabel: yLabel,
+            },
+          },
+        });
+      }
+      break;
     }
 
     default:
-      return newState;
   }
+
+  return newState;
 }
