6d181941feac69651f33cc135f843b9a1ab5a8dc
Max Eddy <max@alleyinteractive.com>
2017-05-30 14:07:25 -0400

    File name consistency

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config.dev');
+const config = require('./dev-webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
