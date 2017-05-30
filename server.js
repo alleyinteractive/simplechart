33a7ecbdd5b35fd69ad79e210fe149a3ea16a7dc
Max Eddy <max@alleyinteractive.com>
2017-05-30 11:01:45 -0400

    Split webpack into environment specific config files, because that seems to be the ideal way to do it.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+const config = require('./webpack.config.dev');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
