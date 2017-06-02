a5e8aa21bb63f11221383d5ba39b67a2f3ae0474
Max Eddy <max@alleyinteractive.com>
2017-06-02 16:45:17 -0400

    Some tests for the request google sheets action.

--- app/actions/requestGoogleSheet.js
+++ app/actions/requestGoogleSheet.js
@@ -4,6 +4,7 @@ import { RECEIVE_ERROR, RECEIVE_RAW_DATA, REQUEST_GOOGLE_SHEET } from '../consta
 
 /**
  * @param {String} sheetId  A google sheets id, or URL containing an id
+ * @return {Promise}
  */
 export default function requestGoogleSheet(sheetId) {
   return (dispatch, getState) => {
@@ -11,7 +12,7 @@ export default function requestGoogleSheet(sheetId) {
 
     if (!googleApiKey) {
       dispatch(actionTrigger(RECEIVE_ERROR, 'e009'));
-      return;
+      return Promise.resolve();
     }
 
     dispatch(actionTrigger(REQUEST_GOOGLE_SHEET));
@@ -23,18 +24,24 @@ export default function requestGoogleSheet(sheetId) {
       },
     };
 
-    fetch(url, options)
-      .then((response) => {
-        if (200 === parseInt(response.status, 10)) {
-          return response.json();
-        }
-
-        throw new Error('Bad Google Sheets API response');
-      })
+    return fetch(url, options)
+      .then(handleResponse)
       .then(toCSV)
       .then((rawData) => dispatch(actionTrigger(RECEIVE_RAW_DATA, rawData)))
       .catch(() => dispatch(actionTrigger(RECEIVE_ERROR, 'e010')));
   };
+}
+
+/**
+ * @param {Response} response
+ * @return {Promise}
+ */
+function handleResponse(response) {
+  if (200 === parseInt(response.status, 10)) {
+    return response.json();
+  }
+
+  throw new Error('Bad Google Sheets API response');
 }
 
 /**
@@ -43,7 +50,7 @@ export default function requestGoogleSheet(sheetId) {
  * @param {String} range
  * @return {String}
  */
-export function buildUrl(sheetId, key, range = 'A1:Z1000') {
+function buildUrl(sheetId, key, range = 'A1:Z1000') {
   return `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${key}`;
 }
 
@@ -51,15 +58,15 @@ export function buildUrl(sheetId, key, range = 'A1:Z1000') {
  * @param {String} input
  * @returns {String}
  */
-export function parseId(input) {
+function parseId(input) {
   const urlMatch = input.match(/\/spreadsheets\/d\/(.+)\//);
   return urlMatch ? urlMatch[1] : input;
 }
 
 /**
- * @param {Object} response
+ * @param {Object} responseData
  * @returns {String}
  */
-export function toCSV(response) {
-  return response.values.reduce((acc, row) => `${acc + row.join(',')}\n`, '');
+function toCSV(responseData) {
+  return responseData.values.reduce((acc, row) => `${acc + row.join(',')}\n`, '');
 }
---
+++ tests/actions/requestGoogleSheet.integration.test.js
@@ -0,0 +1,70 @@
+import actionTrigger from '../../app/actions';
+import requestGoogleSheet from '../../app/actions/requestGoogleSheet';
+import {
+  RECEIVE_ERROR,
+  RECEIVE_RAW_DATA,
+  REQUEST_GOOGLE_SHEET,
+} from '../../app/constants';
+
+jest.mock('isomorphic-fetch');
+
+let fetch, dispatch, getState;
+beforeEach(() => {
+  jest.resetAllMocks();
+  fetch = require('isomorphic-fetch');
+  dispatch = jest.fn();
+  getState = jest.fn();
+});
+
+const mockSheetId = 'abc123';
+
+test('dispatches error if no api key in state', () => {
+  getState.mockReturnValue({});
+
+  const thunk = requestGoogleSheet(mockSheetId);
+  thunk(dispatch, getState).then(() => {
+    expect(dispatch)
+      .toHaveBeenCalledWith(actionTrigger(RECEIVE_ERROR, 'e009'));
+  });
+});
+
+test('handles request to fetch sheet data', () => {
+  const mockResponse = {
+    status: '200',
+    json() {
+      return {
+        values: [
+          ['Group', 'Count'],
+          ['Group A', '3'],
+        ],
+      };
+    },
+  };
+
+  const mockCSV = `Group,Count\nGroup A,3\n`;
+
+  getState.mockReturnValue({ googleApiKey: 'fooBarBaz456' });
+
+  fetch.mockImplementation(() => Promise.resolve(mockResponse));
+
+  const thunk = requestGoogleSheet(mockSheetId);
+  thunk(dispatch, getState).then(() => {
+    expect(dispatch)
+      .toHaveBeenCalledWith(actionTrigger(RECEIVE_RAW_DATA, mockCSV));
+
+    expect(dispatch)
+      .toHaveBeenCalledWith(actionTrigger(REQUEST_GOOGLE_SHEET));
+  });
+});
+
+test('dispatches error if sheet request fails', () => {
+  getState.mockReturnValue({ googleApiKey: 'fooBarBaz456' });
+
+  fetch.mockImplementation(() => Promise.resolve({ status: '500' }));
+
+  const thunk = requestGoogleSheet(mockSheetId);
+  thunk(dispatch, getState).then(() => {
+    expect(dispatch)
+      .toHaveBeenCalledWith(actionTrigger(RECEIVE_ERROR, 'e010'));
+  });
+});
