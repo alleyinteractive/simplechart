1f9f3b67ebc7cbbe837b901d845b01217a95c521
Josh Kadis <josh@alleyinteractive.com>
2017-06-06 06:50:03 -0400

    parse version hash from static filenames

--- dev-site.js
+++ dev-site.js
@@ -2,13 +2,25 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-// Change filename if a version is provided
-const version = process.argv[2] || null;
+// Parse hash from app.1234abc.js
+// Ok to assume there's only one such file in /static
+function getStaticVersion() {
+  return fs.readdirSync(path.join(__dirname, 'static'))
+    .reduce((carry, filename) => {
+      const matches = /^app\.(\w+)\.js$/.exec(filename);
+      return matches ? matches[1] : carry;
+    }, null);
+}
+
+// Update app and vendor filenames with provided version or pick from /static
+const version = process.argv[2] || getStaticVersion();
 if (version) {
   const indexPath = path.join(__dirname, 'index.html');
   const indexHtml = fs.readFileSync(indexPath, 'utf8');
+
+  // replace app.js or app.1234abc.js with new version
   const newHtml = indexHtml.replace(
-    /(app|vendor)\.js/g,
+    /(app|vendor)(?:\.\w+)?\.js/g,
     (match, substr) => `${substr}.${version}.js`
   );
   fs.writeFileSync(indexPath, newHtml);
