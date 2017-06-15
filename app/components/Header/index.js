589f7b54469f9e2ad53de1fddf245b69ba3a6ac4
Max Eddy <max@alleyinteractive.com>
2017-06-15 10:04:03 -0400

    Reset sequence map when closng app.

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -5,10 +5,9 @@ import logoSvg from '!!raw-loader!../../img/simplechartLogo.svg';
 import * as styles from './Header.css';
 import { appSteps } from '../../constants/appSteps';
 import { connect } from 'react-redux';
-import actionTrigger from '../../actions';
+import actionTrigger, { closeApp } from '../../actions';
 import { UPDATE_CURRENT_STEP } from '../../constants';
 import SaveChart from '../SaveChart';
-import { sendMessage } from '../../utils/postMessage';
 import ErrorMessage from './ErrorMessage';
 import { getIsNextStepAvailable } from '../../selectors';
 
@@ -62,7 +61,7 @@ class Header extends Component {
 
   _cancelEdits() {
     if (!this.props.unsavedChanges) {
-      sendMessage('closeApp');
+      this.props.dispatch(closeApp());
       return;
     }
     this.setState({ showUnsavedWarning: true });
@@ -70,7 +69,7 @@ class Header extends Component {
 
   _closeUnsavedWarning(evt) {
     if (evt.target.hasAttribute('data-closeApp')) {
-      sendMessage('closeApp');
+      this.props.dispatch(closeApp());
     }
     this.setState({ showUnsavedWarning: false });
   }
