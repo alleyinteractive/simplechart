a4477de9829af4adb21cae31aa4239320de80347
Max Eddy <max@alleyinteractive.com>
2017-06-22 11:46:03 -0400

    Replace require.ensure with standard dynamic import.

--- app/utils/chartTypeLoader.js
+++ app/utils/chartTypeLoader.js
@@ -2,9 +2,6 @@
  * Async loader for chart types and charting libraries
  */
 export default function chartTypeLoader(componentName) {
-  return new Promise((resolve) => {
-    require.ensure([], () => {
-      resolve(require(`../components/Chart/ChartTypes/${componentName}/index`).default); // eslint-disable-line global-require,import/no-dynamic-require
-    });
-  });
+  return import(`../components/Chart/ChartTypes/${componentName}/index`)
+    .then((module) => module.default);
 }
