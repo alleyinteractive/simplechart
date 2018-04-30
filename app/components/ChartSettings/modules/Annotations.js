53a5ed3ef19a67786cf8213712bf1632bf65ac56
Dan Bowles <dbowles@alleyinterative.com>
2018-04-30 19:36:28 -0400

    Add input fields

--- app/components/ChartSettings/modules/Annotations.js
+++ app/components/ChartSettings/modules/Annotations.js
@@ -5,13 +5,44 @@ import { Button, Divider } from 'rebass';
 import { connect } from 'react-redux';
 import AccordionBlock from '../../Layout/AccordionBlock';
 import actionTrigger from '../../../actions';
-import { EDITING_CHART_ANNOTATIONS } from '../../../constants';
+import DispatchField from '../../lib/DispatchField';
+import {
+  EDITING_CHART_ANNOTATIONS,
+  RECEIVE_REMOVE_ANNOTATION,
+  RECEIVE_UPDATED_ANNOTATION_DATA,
+} from '../../../constants';
 
 class Annotations extends Component {
+  onRemoveClick = (index) => {
+    this.props.dispatch(actionTrigger(RECEIVE_REMOVE_ANNOTATION, index));
+  }
+
   onEditClick = () => {
     const { dispatch, editing } = this.props;
     dispatch(actionTrigger(EDITING_CHART_ANNOTATIONS, !editing));
   };
+
+  handleTitleChange = (item, newValue) => {
+    const { dispatch } = this.props;
+    dispatch(actionTrigger(RECEIVE_UPDATED_ANNOTATION_DATA, {
+      ...item,
+      note: {
+        ...item.note,
+        title: newValue,
+      },
+    }));
+  }
+
+  handleLabelChange = (item, newValue) => {
+    const { dispatch } = this.props;
+    dispatch(actionTrigger(RECEIVE_UPDATED_ANNOTATION_DATA, {
+      ...item,
+      note: {
+        ...item.note,
+        label: newValue,
+      },
+    }));
+  }
 
   render() {
     const editingString = this.props.editing ?
@@ -24,18 +55,34 @@ class Annotations extends Component {
               title={`Annotation ${index + 1}`}
               tooltip={`Settings for Annotation ${index + 1}`}
             >
-              {/* <DispatchField
-                fieldType="Checkbox"
+              <DispatchField
+                fieldType="Input"
                 fieldProps={{
-                  label: 'Locked',
-                  name: `${index}.locked`,
-                  checked: false,
+                  label: 'Annotation Title',
+                  name: `${index}.title`,
+                  value: item.note.title,
                 }}
-                handler={() => {}}
-              /> */}
-              <p><strong>{item.note.title}</strong></p>
-              <p>{item.note.label}</p>
-              <Button theme="error">Remove</Button>
+                handler={(fieldProps, newValue) => {
+                  this.handleTitleChange(item, newValue);
+                }}
+              />
+              <DispatchField
+                fieldType="Input"
+                fieldProps={{
+                  label: 'Annotation Label',
+                  name: `${index}.label`,
+                  value: item.note.label,
+                }}
+                handler={(fieldProps, newValue) => {
+                  this.handleLabelChange(item, newValue);
+                }}
+              />
+              <Button
+                theme="error"
+                onClick={() => this.onRemoveClick(index)}
+              >
+                Remove
+              </Button>
             </AccordionBlock>
           ))
         }
@@ -49,6 +96,7 @@ class Annotations extends Component {
 }
 
 Annotations.propTypes = {
+  // TODO: remove if not using
   // defaultExpand: PropTypes.bool.isRequired,
   dispatch: PropTypes.func.isRequired,
   editing: PropTypes.bool.isRequired,
