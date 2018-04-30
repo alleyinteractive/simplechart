936b3614b6c802c0b492bdb0c0a363090f57b4e0
Dan Bowles <dbowles@alleyinterative.com>
2018-04-30 19:25:02 -0400

    adding remove actions

--- app/reducers/chartAnnotationsReducer.js
+++ app/reducers/chartAnnotationsReducer.js
@@ -1,11 +1,56 @@
 import update from 'immutability-helper';
-import { EDITING_CHART_ANNOTATIONS, RECEIVE_CHART_ANNOTATION } from '../constants';
+import {
+  EDITING_CHART_ANNOTATIONS,
+  RECEIVE_CHART_ANNOTATION,
+  RECEIVE_UPDATED_ANNOTATION_DATA,
+  RECEIVE_REMOVE_ANNOTATION,
+} from '../constants';
 
 export default function chartAnnotationsReducer(state, action) {
   switch (action.type) {
     case RECEIVE_CHART_ANNOTATION: {
+      const annotations =
+        state.chartAnnotations.annotations.slice()
+          .map((annotation, index) => ({ ...annotation, id: index }));
+      annotations.push({
+        ...action.data,
+        id: !annotations.length ? annotations.length : annotations.length - 1,
+      });
+      return update(state, {
+        chartAnnotations: {
+          annotations: { $set: annotations },
+        },
+      });
+    }
+    case RECEIVE_UPDATED_ANNOTATION_DATA: {
       const annotations = state.chartAnnotations.annotations.slice();
-      annotations.push(action.data);
+      const {
+        note,
+        connector,
+        x,
+        y,
+        dx,
+        dy,
+        subject,
+      } = action.data;
+      annotations.splice(action.data.id, 1, {
+        note,
+        connector,
+        x,
+        y,
+        dx,
+        dy,
+        subject,
+      });
+      return update(state, {
+        chartAnnotations: {
+          annotations: { $set: annotations },
+        },
+      });
+    }
+    case RECEIVE_REMOVE_ANNOTATION: {
+      const annotations = state.chartAnnotations.annotations.slice();
+      annotations.splice(action.data, 1);
       return update(state, {
         chartAnnotations: {
           annotations: { $set: annotations },
