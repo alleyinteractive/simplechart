253c7960fd178193aca2c2163f80b7675ddd080e
Dan Bowles <dbowles@alleyinterative.com>
2018-05-15 12:33:30 -0400

    constants for discrete barchart

--- app/constants/chartTypeConfigs/discreteBarChart.js
+++ app/constants/chartTypeConfigs/discreteBarChart.js
@@ -1,11 +1,26 @@
 export const config = {
   type: 'discreteBarChart',
+  annotations: {
+    selector: '.nv-bar rect',
+    container: '.nv-barsWrap',
+    getCoords: (el) => {
+      const transform = d3v4.select(el.node().parentNode)
+        .attr('transform').replace(/.*?\((.*?)\)/g, '$1').split(',')
+        .map((x) => parseFloat(x, 10));
+      const height = parseFloat(el.attr('height'), 10);
+      const width = parseFloat(el.attr('width'), 10);
+
+      return {
+        x: transform[0] + (width / 2),
+        y: transform[1] + (height / 2),
+      };
+    },
+  },
   label: 'Bar Chart',
   dataFormat: 'nvd3SingleSeries',
   componentName: 'NVD3Adapter',
   modules: {
-    settings: ['Annotations'],
-    // settings: ['XAxis', 'YAxis', 'Metadata', 'ColorPalette', 'Annotations'],
+    settings: ['XAxis', 'YAxis', 'Metadata', 'ColorPalette', 'Annotations'],
   },
 };
 
